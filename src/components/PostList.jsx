0adcbcdbccaee174c14c90b4e0ecc4afc05fff27
Laches1 <104304402+Laches1@users.noreply.github.com>
2022-05-03 12:46:23 +0300

    initial commit

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,35 +1,24 @@
-import React from 'react';
+import React from "react";
 import PostItem from "./PostItem";
-import {TransitionGroup, CSSTransition} from "react-transition-group";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-const PostList = ({posts, title, remove}) => {
+const PostList = ({ posts, Token, remove }) => {
+  if (!posts.length) {
+    return <h1 style={{ textAlign: "center" }}>Посты не найдены!</h1>;
+  }
 
-    if (!posts.length) {
-        return (
-            <h1 style={{textAlign: 'center'}}>
-                Посты не найдены!
-            </h1>
-        )
-    }
-
-    return (
-        <div>
-            <h1 style={{textAlign: 'center'}}>
-                {title}
-            </h1>
-            <TransitionGroup>
-                {posts.map((post, index) =>
-                    <CSSTransition
-                        key={post.id}
-                        timeout={500}
-                        classNames="post"
-                    >
-                        <PostItem remove={remove} number={index + 1} post={post} />
-                    </CSSTransition>
-                )}
-            </TransitionGroup>
-        </div>
-    );
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>{Token}</h1>
+      <TransitionGroup>
+        {posts.map((post, index) => (
+          <CSSTransition key={post.id} timeout={500} classNames="post">
+            <PostItem remove={remove} number={index + 1} post={post} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
+    </div>
+  );
 };
 
 export default PostList;
